916147b04bdec2b404f78d5b861bfc5f3e64bae3
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-03-28 22:33:53 -0500

    generate the html file

--- app.js
+++ app.js
@@ -1,14 +1,25 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const profileDataArgs = process.argv.slice(2);
+const [name, github] = profileDataArgs;
+const fs = require("fs");
+const generatePage = require("./src/page-template.js");
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-    console.log(profileDataArr[i]);
-    }
-    console.log("================");
+fs.writeFile("./index.html", generatePage(name, github), err => {
+    if (err) throw err;
 
-    profileDataArr.forEach(profileItem => 
-        console.log(profileItem));
-};
+    console.log("Portfolio complete!  Check out index.html to see the output!");
+});
 
-printProfileData(profileDataArgs);
+
+
+// const printProfileData = profileDataArr => {
+//     // This....
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//     console.log(profileDataArr[i]);
+//     }
+//     console.log("================");
+
+//     // Is the same as this...
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// };
+
+// printProfileData(profileDataArgs);
