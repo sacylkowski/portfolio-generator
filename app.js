b87f4a6d4d1973935c42cba1e6a143ae1663276c
Sacylkowski <sacylkowski@Sarahs-MBP.austin.rr.com>
2022-03-29 21:55:35 -0500

    validating prompts

--- app.js
+++ app.js
@@ -1,14 +1,136 @@
 const inquirer = require("inquirer");
 
-inquirer
-.prompt([
-    {
-        type: "input",
-        name: "name",
-        message: "What is your name?"
-    }
-])
-.then(answers => console.log(answers));
+// inquirer
+// .prompt([
+//     {
+//         type: "input",
+//         name: "name",
+//         message: "What is your name?"
+//     }
+// ])
+// .then(answers => console.log(answers));
+
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your name? (Required)",
+            validate: nameInput => {
+                if (nameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "Enter your Github Username (Required)",
+            validate: githubInput => {
+                if (githubInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your GitHub name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "about",
+            message: "Provide some information about yourself:"
+        }
+    ]);
+};
+
+const promptProject = portfolioData => {
+    console.log(`
+=================
+Add a New Project
+=================
+    `);
+     // if there's no "projects" array property, create one
+     if (!portfolioData.projects) {
+        portfolioData.projects = [];
+        }
+    return inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the name of your project? (Required)",
+            validate: projectNameInput => {
+                if (projectNameInput) {
+                    return true;
+                } else {
+                    console.log("Please enter the project name!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "input",
+            name: "description",
+            message: "Provide a description of the project (Required)",
+            validate: projectDesInput => {
+                if (projectDesInput) {
+                    return true;
+                } else {
+                    console.log("Please enter a description!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "checkbox",
+            name: "languages",
+            message: "What did you build this project with? (Check all that apply)",
+            choices: ["JavaScript", "HTML", "CSS", "ES6", "jQuery", "Bootstrap", "Node"]
+        },
+        {
+            type: "input",
+            name: "link",
+            message: "Enter the GitHub link to your project. (Required)",
+            validate: githubLinkInput => {
+                if (githubLinkInput) {
+                    return true;
+                } else {
+                    console.log("Please enter your link!");
+                    return false;
+                }
+            }
+        },
+        {
+            type: "confirm",
+            name: "feature",
+            message: "Would you like to feature this project?",
+            default: false
+        },
+        {
+            type: "confirm",
+            name: "confirmAddProject",
+            message: "Would you like to enter another project?",
+            default: false
+        }
+    ])
+    .then(projectData => {
+        portfolioData.projects.push(projectData);
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+        } else {
+            return portfolioData;
+        }
+    });
+};
+
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+    console.log(portfolioData);
+});
+
 // const fs = require("fs");
 
 // const generatePage = require("./src/page-template");
